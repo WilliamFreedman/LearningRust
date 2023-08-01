f5805a9f1578467ea95e9b66c079f1e6ab2384b2
WilliamFreedman <freedmanw@bxscience.edu>
2023-08-01 13:15:30 -0400

    added input parsing and comparing to random number

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,6 +1,13 @@
 use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
 
 fn main() {
+
+    let to_guess = rand::thread_rng().gen_range(1..=100);
+
+    println!("Test display {}", to_guess);
+
     println!("Guess the number!");
 
     println!("Please input your guess.");
@@ -10,6 +17,15 @@ fn main() {
     io::stdin()
         .read_line(&mut guess)
         .expect("Failed to read line");
+
+    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+    //converts string to int, .expect handles the error case
+
+    match guess.cmp(&to_guess) { //.cmp returns variants of the Ordering enum, the match then compares them
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win!"),
+    }
 
     println!("You guessed: {guess}");
 }
